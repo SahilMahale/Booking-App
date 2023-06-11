b84dd67bed2c436f68e646a89a33328158ddd3dc
SahilMahale <mahaleasahil@gmail.com>
2023-06-11 21:40:23 +0530

    env vars for db

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Booking-App
 A basic booking app with Go as backend(WIP) and vite/react as front end(soon TM)  
 mainly to practice DB connection with gorm, implement auth from scratch and to learn vite
+
+## Env variables 
+### Database 
+```bash
+export MYSQL_USERNAME=<dbuser>
+export MYSQL_PASSWORD=<dbpass>
+export MYSQL_IP=<dbIP>
+```
--- booking-server/internal/db/db.go
+++ booking-server/internal/db/db.go
@@ -1,6 +1,9 @@
 package db
 
 import (
+	"fmt"
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -13,8 +16,14 @@ type DbConnection struct {
 // }
 
 func NewDBConnection() (DbConnection, error) {
+	uName := os.Getenv("MYSQL_USERNAME")
+	pass := os.Getenv("MYSQL_PASSWORD")
+	dbIP := os.Getenv("MYSQL_IP")
+	if uName == "" || pass == "" || dbIP == "" {
+		panic(fmt.Errorf("one or more ENV variables required for DB connection are not set"))
+	}
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-	dsn := "root:TAa9YiHN4c@tcp(10.42.0.130:3306)/BookigsDatabase?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/BookigsDatabase?charset=utf8mb4&parseTime=True&loc=Local", uName, pass, dbIP)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return DbConnection{}, err
